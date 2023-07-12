3c8b2a9d0d8d60e1ef6010f549f8101a858db08d
tupy77 <tupy77@hotmail.com>
2023-07-12 21:23:35 +0200

    Creacion proyecto, primeras rutas y controlador

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return 'Hello World';
+});
+
+Route::get('/cursos', function () {
+    return 'Cursos';
+});
+
+Route::get('/cursos/create', function () {
+    return 'Crear curso';
+});
+
+Route::get('/cursos/{curso}', function ($curso) {
+    return "Curso: $curso";
+});

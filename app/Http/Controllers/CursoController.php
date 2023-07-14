75e10cf786374ed27633bf52c747ed798ddc2efc
tupy77 <tupy77@hotmail.com>
2023-07-14 08:01:43 +0200

    Primeras vistas

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,16 +8,18 @@ class CursoController extends Controller
 {
     public function index()
     {
-        return 'Cursos';
+        return view('cursos.index');
     }
 
     public function create()
     {
-        return 'Crear curso';
+        return view('cursos.create');
     }
 
     public function show($curso)
     {
-        return "Curso: $curso";
+        return view('cursos.show', ['curso' => $curso]);
+        // OTRO MODO
+        //return view('cursos.show', compact('curso'));
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function __invoke() // __invoke() is a magic method
     {
-        return 'Hello World desde el controller';
+        return view('home');
     }
 }

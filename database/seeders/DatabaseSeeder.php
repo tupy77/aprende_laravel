bb10c29a14df40d41f27522f730f00f344a3187c
tupy77 <tupy77@hotmail.com>
2023-07-16 01:09:35 +0200

    Seeders base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Curso;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //ASI SE CREAN REGISTROS EN LA BASE DE DATOS PERO NO ES RECOMENDABLE YA QUE SE RALIZAN DE 1 EN 1
+        $curso = new Curso();
+
+        $curso->name = 'Laravel';
+        $curso->description = 'El mejor framework de PHP';
+        $curso->category = 'Desarrollo web';
+
+        $curso->save();
+
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
     }
 }

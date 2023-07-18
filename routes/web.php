517f1fb570d6d86ae039f193d3dd01f26d79ae32
tupy77 <tupy77@hotmail.com>
2023-07-18 03:38:48 +0200

    Agrupar rutas y cambiar endpoints

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,15 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', HomeController::class);
 
-Route::controller(CursoController::class)->group(function () {
-    Route::get('cursos', 'index')->name('cursos.index'); //nombre de la ruta
-    Route::get('cursos/create', 'create')->name('cursos.create');
-    Route::post('cursos', 'store')->name('cursos.store');
-    Route::get('cursos/{curso}', 'show')->name('cursos.show');
-    Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
-    Route::put('cursos/{curso}', 'update')->name('cursos.update');
-    Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy'); // COMO PONER SE PODRIA PONER EL METODO PUT, PERO SE REPETIRIA LA URL Y NO SERIA CORRECTO. POR ESO SE PONE DELETE.
-});
+// Route::controller(CursoController::class)->group(function () {
+//     Route::get('cursos', 'index')->name('cursos.index'); //nombre de la ruta
+//     Route::get('cursos/create', 'create')->name('cursos.create');
+//     Route::post('cursos', 'store')->name('cursos.store');
+//     Route::get('cursos/{curso}', 'show')->name('cursos.show');
+//     Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
+//     Route::put('cursos/{curso}', 'update')->name('cursos.update');
+//     Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy'); // COMO PONER SE PODRIA PONER EL METODO PUT, PERO SE REPETIRIA LA URL Y NO SERIA CORRECTO. POR ESO SE PONE DELETE.
+// });
 
-
-//TAMBIEN SE PUEDE HACER ASI
-// Route::group(['prefix' => 'cursos'], function () {
-//         Route::get('/', [CursoController::class, 'index']); //CADA UNO DE ESTOS GET SE PUEDEN PONER POR INDIVIDUAL
-//         Route::get('create', [CursoController::class, 'create']);
-//         Route::get('{curso}', [CursoController::class, 'show']);
-//     });
-
+Route::resource('cursos', CursoController::class); //ESTO ES LO MISMO QUE LO DE ARRIBA, PERO MAS RESUMIDO
+// Esto analiza el nombre de la ruta y lo asocia con el metodo del controlador, creando las rutas necesarias para el CRUD

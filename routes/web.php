74e33559adeff5dbf2f06772adf9c67acdb37aaf
tupy77 <tupy77@hotmail.com>
2023-07-18 12:30:35 +0200

    Cambiar base del nombre de la ruta y parametros

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,4 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', HomeController::class);
 
-// Route::controller(CursoController::class)->group(function () {
-//     Route::get('cursos', 'index')->name('cursos.index'); //nombre de la ruta
-//     Route::get('cursos/create', 'create')->name('cursos.create');
-//     Route::post('cursos', 'store')->name('cursos.store');
-//     Route::get('cursos/{curso}', 'show')->name('cursos.show');
-//     Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
-//     Route::put('cursos/{curso}', 'update')->name('cursos.update');
-//     Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy'); // COMO PONER SE PODRIA PONER EL METODO PUT, PERO SE REPETIRIA LA URL Y NO SERIA CORRECTO. POR ESO SE PONE DELETE.
-// });
-
-Route::resource('cursos', CursoController::class); //ESTO ES LO MISMO QUE LO DE ARRIBA, PERO MAS RESUMIDO
-// Esto analiza el nombre de la ruta y lo asocia con el metodo del controlador, creando las rutas necesarias para el CRUD
+Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos'); // SI NO SE PONE PARAMETERS DARIA ERROR, YA QUE MODIFICAMOS EL NOMBRE DE LA URL Y EL DE LOS  PARAMETROS QUE SE ENVIAN, O SE CAMBIAN TODO ESO EN TODAS LAS VIEWS, O SE PONE LA FUNCION PARAMETERS PARA CAMBIARLO Y DEJAR QUE SIGAN COMO ESTABAN//->parameters(['asignaturas' => 'curso']); //->only(['index', 'show', 'create', 'store', 'edit', 'update', 'destroy']); //->except(['edit', 'update', 'destroy']);

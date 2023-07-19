6cc6bc329d7f562401e81ea2d749f03e2ed4794f
tupy77 <tupy77@hotmail.com>
2023-07-19 04:10:58 +0200

    configurar proyecto con breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CursoController;
-
-use App\Http\Controllers\ContactanosController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +14,18 @@ use App\Http\Controllers\ContactanosController;
 |
 */
 
-Route::get('/', HomeController::class)->name('home');
-
-Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos');
-
-Route::view('nosotros', 'nosotros')->name('nosotros');
-
-Route::controller(ContactanosController::class)->prefix('contactanos')->group(function () {
-    Route::get('/', 'index')->name('contactanos.index');
-    Route::post('/', 'store')->name('contactanos.store');
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

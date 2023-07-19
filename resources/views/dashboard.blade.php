e174f27d62b800df13b2e90b4620b3a04fd6173b
tupy77 <tupy77@hotmail.com>
2023-07-20 01:25:29 +0200

    Componentes, pasar con codigo PHP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
+
+                @php
+                    $title = 'Transactions every 24 hours';
+                    $description = 'This is a description';
+                    $type = 'danger';
+                    $alert_header = 'Alerta';
+                    $alert_text = 'Este es un texto de alerta';
+                @endphp
+
+                {{-- Cuidado con los dos puntos antes del nombre de la variable --}}
+                <x-stats :title="$title" :description="$description" :type="$type" :alert_header="$alert_header" :alert_text="$alert_text">
+                    44 million
+                </x-stats>
+
+                
                 <x-stats>
                     44 million
 

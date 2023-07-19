3482cff89ee169ba93dc8ed87dda9a3ae33e9b7c
tupy77 <tupy77@hotmail.com>
2023-07-19 02:25:53 +0200

    Emails 1

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
 
+use App\Mail\ContactanosMailable;
+use Illuminate\Support\Facades\Mail;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +23,9 @@ Route::get('/', HomeController::class)->name('home');
 Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos'); // SI NO SE PONE PARAMETERS DARIA ERROR, YA QUE MODIFICAMOS EL NOMBRE DE LA URL Y EL DE LOS  PARAMETROS QUE SE ENVIAN, O SE CAMBIAN TODO ESO EN TODAS LAS VIEWS, O SE PONE LA FUNCION PARAMETERS PARA CAMBIARLO Y DEJAR QUE SIGAN COMO ESTABAN//->parameters(['asignaturas' => 'curso']); //->only(['index', 'show', 'create', 'store', 'edit', 'update', 'destroy']); //->except(['edit', 'update', 'destroy']);
 
 Route::view('nosotros', 'nosotros')->name('nosotros'); // Se pone view en vez de get, ya que no se va a hacer ninguna consulta a la base de datos, solo se va a mostrar una vista, pagina estatica...etc
+
+Route::get('contactanos', function () {
+        $correo = new ContactanosMailable;
+        Mail::to('tupy1977@gmail.com')->send($correo); // Se envia el correo a la direccion que se pone en el to
+        return "Mensaje enviado";
+    })->name('contactanos');

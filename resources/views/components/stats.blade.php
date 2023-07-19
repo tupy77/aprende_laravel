6333d8ca06f2f362817ae512b104ee6b9d219a08
tupy77 <tupy77@hotmail.com>
2023-07-20 01:18:34 +0200

    Components, mas sobre paso de parametros

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -1,21 +1,46 @@
-@props(['title', 'description'])
+@props(['title', 'description', 'type', 'alert_header', 'alert_text'])
+{{-- @props(['title', 'description', 'type' => 'info', 'alert_header', 'alert_text']) // Con esto le damos un valor por defecto a type --}}
+
+@php
+    switch ($type) {
+        case 'info':
+            $classes = 'bg-blue-100 border-blue-400 text-blue-700';
+            break;
+
+        case 'danger':
+            $classes = 'bg-red-100 border-red-400 text-red-700';
+            break;
+        
+        default:
+            $classes = 'bg-green-100 border-green-400 text-green-700';
+            break;
+    }
+
+@endphp
 
 <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
-        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-          <dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
-        </div>
-        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-          <dt class="text-base leading-7 text-gray-600">Assets under holding</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">$119 trillion</dd>
-        </div>
-        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-          <dt class="text-base leading-7 text-gray-600">{{ $title }}</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ $slot }}</dd>
-          <p>{{$description}}</p>
-        </div>
-      </dl>
+        <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+            <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                <dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
+            </div>
+            <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                <dt class="text-base leading-7 text-gray-600">Assets under holding</dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">$119 trillion
+                </dd>
+            </div>
+            <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                <dt class="text-base leading-7 text-gray-600">{{ $title }}</dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                    {{ $slot }}</dd>
+                <p>{{ $description }}</p>
+            </div>
+        </dl>
     </div>
-  </div>
+</div>
+
+<article class=" border-1-4 p-4 {{ $classes }} " role="alert">
+    <h1 class="font-bold">{{ $alert_header }}</h1>
+    <p>{{ $alert_text }}</p>
+</article>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,9 +10,21 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
                 <x-stats>
-                    <x-slot name="title">Transactions every 24 hours</x-slot>
-                    <x-slot name="description">This is a description</x-slot>
                     44 million
+
+                    <x-slot name="description">This is a description</x-slot>
+
+                    <x-slot name="title">
+                        <div>
+                            <h1>Asi es mas facil enviar cosas mas complejas</h1>
+                            <p>Ves</p>
+                        </div>
+                    </x-slot>
+
+                    <x-slot name="type">info</x-slot>
+                    <x-slot name="alert_header">Alerta</x-slot>
+                    <x-slot name="alert_text">Este es un texto de alerta</x-slot>
+                    
                 </x-stats>
             </div>
         </div>
